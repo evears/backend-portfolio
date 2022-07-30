434dde4dd13ce5feb3bfccd7fbc8a7e4e1e44c50
Evelyn Arias <eve.arias91@gmail.com>
2022-07-30 18:27:14 -0300

    crossorigin

--- portfolio/src/main/java/com/portfolio/portfolio/controller/HabilidadController.java
+++ portfolio/src/main/java/com/portfolio/portfolio/controller/HabilidadController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolioapp-9415a.web.app")
 public class HabilidadController {
     
     @Autowired
